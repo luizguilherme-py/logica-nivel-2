325d488120c454ac2228a8751e4004c5d2fbb2e4
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-17 19:41:31 -0300

    Create exec-5.py

---
+++ exec-5.py
@@ -0,0 +1,8 @@
+#Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+area = base * altura
+
+print("A áre do retãngulo é:", area)
