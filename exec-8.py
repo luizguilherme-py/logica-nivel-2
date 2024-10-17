97de56b897a9bff29041b4a88d38d7b871dcd755
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-17 19:01:09 -0300

    Create exec-8.py

---
+++ exec-8.py
@@ -0,0 +1,16 @@
+votos_nulos = int(input("Qual o total de votos nulos? "))
+
+votos_validos = int(input("Qual ao total de votos validos? ")) #variaveis 
+
+votos_brancos = int(input("Qual o total de votos brancos? "))
+
+totalDeVotos = votos_nulos + votos_validos + votos_brancos
+
+porcentagemValidos = (votos_validos / totalDeVotos ) * 100
+print(porcentagemValidos)
+
+porcentagemNulos = ( votos_nulos / totalDeVotos ) * 100 # calculo
+print(porcentagemNulos)
+
+porcentagemBrancos = (votos_brancos / totalDeVotos ) * 100
+print(porcentagemBrancos)
