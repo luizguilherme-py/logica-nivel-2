914a1c5801e0c3a23f0453b1cb92d5e8534838ea
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-17 20:12:58 -0300

    Update exec-7.py

--- exec-7.py
+++ exec-7.py
@@ -1,5 +1,16 @@
-ano = int(input("Digite o ano que você deseja chegar:"))
-mes = int(input("Digite o mês do ano atual:"))                  
-dias = int(input("Digite o dia do mês atual:"))
-atual = (ano * mes * dias)
-print("A idade total em dias é:",atual)
+#Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores
+
+total_eleitores = int(input("Digite o número total de eleitores: "))
+votos_brancos = int(input("Digite o número de votos brancos: "))
+votos_nulos = int(input("Digite o número de votos nulos: "))
+votos_validos = int(input("Digite o número de votos válidos: "))
+
+# Passo 2: Calcular os percentuais
+percentual_brancos = (votos_brancos / total_eleitores) * 100
+percentual_nulos = (votos_nulos / total_eleitores) * 100
+percentual_validos = (votos_validos / total_eleitores) * 100
+
+# Passo 3: Exibir os percentuais
+print("Percentual de votos brancos: {:.2f}%".format(percentual_brancos))
+print("Percentual de votos nulos: {:.2f}%".format(percentual_nulos))
+print("Percentual de votos válidos: {:.2f}%".format(percentual_validos))
