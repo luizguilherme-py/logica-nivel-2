eaa6f659b9acddfc1f2ab71326cf10e7d5adddc7
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-17 21:20:20 -0300

    Update exec-12.py

--- exec-12.py
+++ exec-12.py
@@ -1,1 +1,16 @@
+#Faça um algoritmo que leia três notas de um aluno, calcule e escreva a média final deste aluno. Considerar que a média é ponderada e que o peso das notas é 2, 3 e 5. Fórmula para o cálculo da média final é:
 
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+
+peso1 = 2
+peso2 = 3
+peso3 = 5
+
+
+media_final = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
+
+
+print("A média final do aluno é: {:.2f}".format(media_final))
