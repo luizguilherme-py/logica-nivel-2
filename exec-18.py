2aaf26d7d2e5db2fba5d075d449035c5afe7d78f
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-18 14:48:24 -0300

    Create exec-18.py

---
+++ exec-18.py
@@ -0,0 +1,10 @@
+#Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+if valor1 > valor2:
+  print(f"O maior valor é: {valor1}")
+else:
+  print(f"O maior valor é {valor2}")
+  
