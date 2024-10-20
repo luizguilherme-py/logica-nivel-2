56da3d9ccd70fda26bf1bd43189434b4a92bed6e
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-20 13:47:20 -0300

    Create exec-27.py

---
+++ exec-27.py
@@ -0,0 +1,17 @@
+# Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+
+valor1 = float(input("Digite o primeiro valr: "))
+valor2 = float(input("Digite o segundo valor: "))
+valor3 = float(input("Digite o terceiro valor: "))
+
+if valor1 > valor2:
+  if valor1 > valor3
+      print(f"O maior valor é: {valor1}")
+  else:
+      print(f"O maior valor é: {3}")
+else:
+  if valor2 > valor3:
+      print(f"O maior valor é: {valor2}")
+  else:
+      print(f"O maior valor é: {valor3}")
+  
