ba794a07bf958fa35c9eb0b55420fded45bc52dc
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-31 21:17:53 -0300

    Update exec-14.py

--- exec-14.py
+++ exec-14.py
@@ -1,3 +1,5 @@
+#Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+
 valor = float(input("Digite um valor: "))
 if valor >= 0:
   print("O valor é POSITIVO.")
