14f5952c1e60a17f091d00a0916b510b310ab5be
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-10-31 21:18:46 -0300

    Update exec-34.py

--- exec-34.py
+++ exec-34.py
@@ -1,5 +1,3 @@
-# Um posto está vendendo combustíveis com a seguinte tabela de descontos: 
-Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90. 
 
 preco_alcool = 2.90
 preco_gasolina = 3.30
@@ -8,18 +6,25 @@ litros = float(input("Digite a quantidade de litros vendidos: "))
 tipo_combustivel = input("Digite o tipo de combustível (A para álcool, G para gasolina): ").upper()
 
 if tipo_combustivel == 'A':
-  if litros <= 20:
-    desconto = 0.03 
-else:
-    desconto = 0.05
-preco_total = litros * preco_alcool * (1 - desconto)
+    
+    if litros <= 20:
+        desconto = 0.03  
+    else:
+        desconto = 0.05  
+    preco_total = litros * preco_alcool * (1 - desconto)
 
 elif tipo_combustivel == 'G':
-  if litros <= 20:
-    desconto = 0.04
-else: 
-    desconto = 0.06
-preco_total = 0
+    
+    if litros <= 20:
+        desconto = 0.04  
+    else:
+        desconto = 0.06  
+    preco_total = litros * preco_gasolina * (1 - desconto)
+
+else:
+    print("Tipo de combustível inválido.")
+    preco_total = 0  
+
 
 if preco_total > 0:
     print(f"Valor a ser pago: R$ {preco_total:.2f}")
