083269596cd54d67f99cd04394e6684cb130204b
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-11-02 13:26:28 -0300

    Update exec-40.py

--- exec-40.py
+++ exec-40.py
@@ -1,15 +1,21 @@
+#Faça um algoritmo para ler: a descrição do produto (nome), a quantidade adquirida e o preço unitário. Calcular e escrever o total (total = quantidade adquirida * preço unitário), o desconto e o total a pagar (total a pagar = total - desconto), sabendo-se que: 
+
+#- Se quantidade <= 5 o desconto será de 2%
+#- Se quantidade > 5 e quantidade <=10 o desconto será de 3%
+#- Se quantidade > 10 o desconto será de 5%
+
 produto = input("Digite a descrição do produto: ")
 quantidade = int(input("Digite a quantidade adquirida: "))
 preco_unitario = float(input("Digite o preço unitário: "))
 
 total = quantidade * preco_unitario
 
-if quantidade > 100:
-    desconto = total * 0.10
-elif quantidade >= 50 and quantidade <= 100:
+if quantidade <= 5:
+    desconto = total * 0.02
+elif quantidade > 5 and quantidade <= 10:
+    desconto = total * 0.03
+else:  
     desconto = total * 0.05
-else:
-    desconto = 0
 
 total_a_pagar = total - desconto
 
