75c5796d043b8794b6e592abe984f8b42b3d6592
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-11-02 14:23:00 -0300

    Update exec-45.py

--- exec-45.py
+++ exec-45.py
@@ -4,7 +4,7 @@ valor1 = float(input("Digite o primeiro valor: "))
 valor2 = float(input("Digite o segundo valor (não pode ser zero): "))
 
 while valor2 == 0:
-    print("Valor inválido! O segundo valor deve ser diferente de zero.")
+    print("O segundo valor deve ser diferente de zero.")
     valor2 = float(input("Digite o segundo valor (não pode ser zero): "))
 
 resultado = valor1 / valor2
