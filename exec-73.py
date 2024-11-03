96e594291ba26d64fb388938b736b7134feb63c7
luiz guilherme alves moreira marques <luizlima06060@gmail.com>
2024-11-03 17:09:58 -0300

    Update exec-73.py

--- exec-73.py
+++ exec-73.py
@@ -3,20 +3,40 @@
 total_salarios = 0
 total_filhos = 0
 quantidade_habitantes = 0
+maior_salario = float('-inf')  
+pessoas_com_salario_baixo = 0  # 
 
 while True:
-    salario = float(input("Digite o salário do habitante (Ou -1 para encerrar): "))
-    if salario == -1:
-        break
-    filhos = int(input("Digite o número de filhos do habitante: "))
     
+    salario = float(input("Digite o salário do habitante (salário negativo para encerrar): "))
+    
+
+    if salario < 0:
+        break
+
+
+    filhos = int(input("Digite o número de filhos do habitante: "))
+
+
     total_salarios += salario
     total_filhos += filhos
     quantidade_habitantes += 1
-    
-media_salarios = total_salarios / quantidade_habitantes if quantidade_habitantes > 0 else 0
 
-print(f"\nTotal de habitantes pesquisados: {quantidade_habitantes}")
-print(f"Soma total dos salários: R$ {total_salarios:.2f}")
-print(f"Média dos salários: R$ {media_salarios:.2f}")
-print(f"Soma total de filhos: {total_filhos}")
+
+    if salario > maior_salario:
+        maior_salario = salario
+
+
+    if salario < 150:
+        pessoas_com_salario_baixo += 1
+
+
+media_salarios = total_salarios / quantidade_habitantes if quantidade_habitantes > 0 else 0
+media_filhos = total_filhos / quantidade_habitantes if quantidade_habitantes > 0 else 0
+percentual_salario_baixo = (pessoas_com_salario_baixo / quantidade_habitantes * 100) if quantidade_habitantes > 0 else 0
+
+
+print(f"\nMédia de salário da população: R$ {media_salarios:.2f}")
+print(f"Média do número de filhos: {media_filhos:.2f}")
+print(f"Maior salário dos habitantes: R$ {maior_salario:.2f}")
+print(f"Percentual de pessoas com salário menor que R$ 150,00: {percentual_salario_baixo:.2f}%")
